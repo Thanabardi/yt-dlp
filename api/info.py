8f013e37743ad79a71bf2df58715335fb30e14e1
Thanabardi <thanabardi@hotmail.com>
2024-02-27 02:42:16 +0700

    add yt-dlp

--- api/info.py
+++ api/info.py
@@ -1,19 +1,37 @@
 from http.server import BaseHTTPRequestHandler
-from urllib.parse import urlparse, parse_qs
 import json
+import yt_dlp
+
+
+def video_info(platform, id):
+    match platform:
+        case "youtube":
+            url = f"https://www.youtube.com/watch?v={id}"
+
+    with yt_dlp.YoutubeDL() as ydl:
+        info = ydl.extract_info(url, download=False)
+        return ydl.sanitize_info(info)
 
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-        print(self.path)
-        print(urlparse(self.path))
-        print(urlparse(self.path).query)
-        print(parse_qs(urlparse(self.path).query))
-        query = parse_qs(urlparse(self.path).query)
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        response = json.dumps(query)
-        self.wfile.write(response.encode('utf-8'))
+        try:
+            query_str = self.path.split("?", 1)[1]
+            query = dict(q.split("=") for q in query_str.split("&"))
+        except:
+            self.send_error(422, "invalid parameters")
+            return
+
+        if "platform" not in query or "id" not in query:
+            self.send_error(422, "invalid parameters")
+            return
+
+        try:
+            response = video_info(query["platform"], query["id"])
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(response).encode('utf-8'))
+        except Exception as e:
+            self.send_error(500, e)
         return
