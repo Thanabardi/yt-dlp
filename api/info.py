e8118ab5a90a08383d7044779204b444df4a6486
Thanabardi <thanabardi@hotmail.com>
2024-02-26 22:57:10 +0700

    add print

--- api/info.py
+++ api/info.py
@@ -2,9 +2,14 @@ from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 import json
 
+
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        print(self.path)
+        print(urlparse(self.path))
+        print(urlparse(self.path).query)
+        print(parse_qs(urlparse(self.path).query))
         query = parse_qs(urlparse(self.path).query)
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
