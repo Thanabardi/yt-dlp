dc95e6bf2d856a4fa57b211d199f18581d633297
Thanabardi <thanabardi@hotmail.com>
2024-02-26 22:39:54 +0700

    add path parse

--- api/info.py
+++ api/info.py
@@ -1,11 +1,13 @@
 from http.server import BaseHTTPRequestHandler
-
+from urllib.parse import urlparse, parse_qs
+import json
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        query = parse_qs(urlparse(self.path).query)
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(self.path.encode('utf-8'))
+        self.wfile.write(json.dumps(query).encode('utf-8'))
         return
